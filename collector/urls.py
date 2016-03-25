cf6094e13e5fdd9f8575ee4cc1470156c6c537fc
aczapata <andreaczapata@gmail.com>
2016-03-25 15:10:25 -0500

    Style and other things

--- collector/urls.py
+++ collector/urls.py
@@ -4,15 +4,12 @@ from . import views
 app_name="collector"
 urlpatterns = [
     # Examples:
-    
-    
     url(r'^$', views.index, name='index'),
+    url(r'^load/$', views.load_tweets, name='load_tweets'),
+    url(r'^statistics/$', views.tweets_tokenize, name='statistics'),
    	url(r'^(?P<tweet_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<tweet_id>[0-9]+)/results/$', views.results, name='results'),
-    url(r'^(?P<qtweet_id>[0-9]+)/vote/$', views.vote, name='vote'),
-	url(r'^load/$', views.load_tweets, name='load_tweets'),
+    url(r'^(?P<tweet_id>[0-9]+)/tokenize/$', views.tweet_tokenize, name='tokenize'),
+    url(r'^(?P<tweet_id>[0-9]+)/vote/$', views.vote, name='vote'),
+	
 ]
-
-"""
-   
-    """
--- collector/views.py
+++ collector/views.py
@@ -1,18 +1,61 @@
-import time
-import datetime
+import time, datetime
 import json
+import re,string,operator
 
+from collections import Counter
 from django.shortcuts import render,get_object_or_404,HttpResponse,HttpResponseRedirect
 from .models import TwitterData,Sentiment
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse,HttpResponseRedirect
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
-# Create your views here.
+# Variables
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+    
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
 
+
+# Methods.
 def index(request):
     tweets_list = TwitterData.objects.all()
     context = {'tweets_list': tweets_list}
     return render(request, 'collector/index.html', context)
+
+def tweets_tokenize(request):
+    tweets_list = TwitterData.objects.all()
+    for tweet in tweets_list:
+        # Create a list with all the terms
+        terms_all = [term for term in preprocess(tweet.content)]
+        punctuation = list(string.punctuation)
+        stop = stopwords.words('english') + punctuation + ['rt', 'via']
+        terms_stop = [term for term in terms_all if term not in stop]
+        count_all = Counter()
+        count_all.update(terms_stop)
+        # Update the counter
+        count_all.update(terms_all)
+    common_terms = count_all.most_common(10)
+    context = {'tweets_list': tweets_list, 'count_all': count_all, 'common_terms': common_terms}
+    return render(request, 'collector/statistics.html', context)
 
 def detail(request, tweet_id):
     tweet= get_object_or_404(TwitterData, pk=tweet_id)
@@ -41,6 +84,29 @@ def results(request, tweet_id):
     tweet = get_object_or_404(TwitterData, pk=tweet_id)
     return render(request, 'collector/results.html', {'tweet': tweet})
 
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+def tweet_tokenize(request,tweet_id):
+    
+    tweet = get_object_or_404(TwitterData, pk=tweet_id)
+    tweet_tokenized=preprocess(tweet.content)
+    terms_all = [term for term in tweet_tokenized]
+    punctuation = list(string.punctuation)
+    stop = stopwords.words('english') + punctuation + ['rt', 'via']
+    terms_stop = [term for term in terms_all if term not in stop]
+    count_all = Counter()
+    count_all.update(terms_stop)
+    context= {'tweet_tokenized': tweet_tokenized, 'terms_stop': terms_stop,'tweet': tweet, 'count_all':count_all}
+    return render(request, 'collector/tokenize.html', context)
+ 
+
 def load_tweets(request):
     tweets_data_path = '../TwitterData240320161624.txt'
     tweets_file = open(tweets_data_path, "r")
@@ -65,9 +131,6 @@ def load_tweets(request):
             tweet_db.sentiment_set.create(sentiment_text="UND")
             tweet_db.sentiment_set.create(sentiment_text="NEG")
             tweet_db.sentiment_set.create(sentiment_text="POS")
-            
-            
-            
         except:
             continue
     return HttpResponse('Finished!\n' +str(count)+" Tweets loaded")
