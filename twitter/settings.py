64ea93334e82230f3458a5f22c233eaf3a18cf2e
aczapata <andreaczapata@gmail.com>
2016-04-02 16:11:34 -0500

    More config

--- twitter/settings.py
+++ twitter/settings.py
@@ -15,6 +15,7 @@ import os
 import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
