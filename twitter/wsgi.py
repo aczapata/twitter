f35fa3cebe64300322fefd5fd6098d68001ca1eb
aczapata <andreaczapata@gmail.com>
2016-04-08 23:16:08 -0500

    Celery Production

--- twitter/wsgi.py
+++ twitter/wsgi.py
@@ -10,12 +10,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
+import djcelery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitter.settings")
 
 application = get_wsgi_application()
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+djcelery.setup_loader()
